8b63be01bbf185bd866b41a245d8857c9c2a9f68
Felix <60808107+ItsFelix5@users.noreply.github.com>
2025-09-27 14:54:16 +0200

    Haha oops

--- src/main/java/dev/vaito/LittleAdventure.java
+++ src/main/java/dev/vaito/LittleAdventure.java
@@ -6,8 +6,12 @@ import dev.vaito.items.ModItems;
 import dev.vaito.screen.ModScreenHandler;
 import net.fabricmc.api.ModInitializer;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 public class LittleAdventure implements ModInitializer {
     public static final String MOD_ID = "littleadventure";
+    public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
 
     @Override
     public void onInitialize() {
